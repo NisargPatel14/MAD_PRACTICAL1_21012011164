b6da0293add6c03d90304313f8847de5b4c4a164
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2023-07-17 10:40:02 +0530

    P1 Final

--- src/main/kotlin/PRACTICAL1.1.kt
+++ src/main/kotlin/PRACTICAL1.1.kt
@@ -1,4 +1,23 @@
 fun main()
 {
-    println("hello world")
+    val a:Int =22
+    val b:Float=1.5F
+    val c:Char = 'T'
+    val d:String ="NJP"
+    val e:Boolean=true
+    val f:Double=101.23
+    val g:Long=836619230
+    val h:Short=-23
+    val i:Byte = 126
+
+    println("$a is integer")
+    println("$b is float")
+    println("$c is character")
+    println("$d is string")
+    println("$e is boolean")
+    println("$f is double")
+    println("$g is long")
+    println("$h is short")
+    println("$i is byte")
+
 }
