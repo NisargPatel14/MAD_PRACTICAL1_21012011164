8a26056ef4acd7bd92f42ad02600e18c65b6e58d
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2023-07-18 09:33:15 +0530

    P4 Final

---
+++ src/main/kotlin/PRACTICAL1.4.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+fun main(){
+    print("Enter a number: ")
+    val sc = Scanner(System.`in`)
+    val a=sc.nextInt()
+    if(a%2==0){
+        print("Even Number")
+    }
+    else{
+        print("Odd Number")
+    }}
