77055449a9741703cbd661eb872781bdf125c622
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2023-07-18 09:57:09 +0530

    P5 Final

--- src/main/kotlin/PRACTICAL1.5.kt
+++ src/main/kotlin/PRACTICAL1.5.kt
@@ -1,19 +1,20 @@
 fun main(){
     print("Enter Month Number:")
-    val month= readLine()
+    val month= readLine()?.toInt()!!
     when(month){
-        "1"->print("January")
-        "2"->print("February")
-        "3"->print("March")
-        "4"->print("April")
-        "5"->print("May")
-        "6"->print("June")
-        "7"->print("July")
-        "8"->print("August")
-        "9"->print("September")
-        "10"->print("October")
-        "11"->print("November")
-        "12"->print("December")
+        1->print("January")
+        2->print("February")
+        3->print("March")
+        4->print("April")
+        5->print("May")
+        6->print("June")
+        7->print("July")
+        8->print("August")
+        9->print("September")
+        10->print("October")
+        11->print("November")
+        12->print("December")
+        else ->print("Invalid Number")
     }
 
 }
